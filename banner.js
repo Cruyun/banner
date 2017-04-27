59e0233f24862eb9432800fd885b60c4977308c7
Cruyun <1045671796@qq.com>
2017-04-27 23:03:39 +0800

    begin

---
+++ banner.js
@@ -0,0 +1,16 @@
+var pic = document.getElementById('pic');
+    picture= pic.getelmEentByTagName('div');
+
+var slide_left = document.getElementById('arrow-left');
+var slide_right = document.getElementById('arrow-right');
+
+var firstNum=0;
+
+slide_button.addEventListener('click',function({
+
+}))
+
+
+//object.getAttribute(attribute)获取属性
+//object.setAttribute(attribute,value)修改属性
+//Event.target
